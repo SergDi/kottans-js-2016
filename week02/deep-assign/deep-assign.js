dffcd3b7c3659c02cf8dac62a80ac3c6473557e3
sergkondrashov <sergkondrashov@gmail.com>
2016-06-14 08:04:40 +0300

    add filter hasOwnProperty

--- week02/deep-assign/deep-assign.js
+++ week02/deep-assign/deep-assign.js
@@ -1,9 +1,13 @@
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+const propertyIsEnumerable = Object.prototype.propertyIsEnumerable;
+
 function deepAssign(target, ...sources) {
   if (target === null) throw new TypeError('not null');
 
   sources.forEach(source => {
     Reflect.ownKeys(source)
-      .filter(key => { return source.propertyIsEnumerable(key) })
+      .filter(key => { return propertyIsEnumerable.call(source, key) })
+      .filter(key => { return !hasOwnProperty.call(target, key) })
       .forEach(key => {
         if (typeof source[key] === 'object' && source[key] !== null) {
           target[key] = new source[key].constructor();
